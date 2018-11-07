a0c88d8645bd634aa07d257364e775a94ed650a6
Vladimir Antonic <vladimir.a@vivifyideas.com>
2018-11-07 11:26:32 +0100

    #LVAI-23 Backend signup completed

--- toDoApp/routes/api.php
+++ toDoApp/routes/api.php
@@ -28,5 +28,6 @@ function($router){
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
+    Route::post('register','MyRegisterController@store');
 });
 
