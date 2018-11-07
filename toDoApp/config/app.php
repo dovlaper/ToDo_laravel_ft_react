e6728f9c21c4e3a578cb0b5cdf88fd63a6ff657f
Vladimir Antonic <vladimir.a@vivifyideas.com>
2018-11-07 10:19:12 +0100

    #LVAI-23 JWT token implemented, login works.

--- toDoApp/config/app.php
+++ toDoApp/config/app.php
@@ -173,6 +173,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+        * Jwt Token Provider
+        */
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+
     ],
 
     /*
@@ -221,7 +226,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
     ],
 
 ];
